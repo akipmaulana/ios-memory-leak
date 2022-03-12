33084e9867bfcbb43c96ee08cf693da2e0675688
Akip Maulana <akip.maulana@gmail.com>
2022-03-12 10:27:32 +0700

    Initial Commit

---
+++ MemoryLeak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoryLeak
+//
+//  Created by Akirah Dev on 12/03/22.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
